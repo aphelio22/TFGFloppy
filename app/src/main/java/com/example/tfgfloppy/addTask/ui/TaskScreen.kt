950393e0be7ce15836630604145e90d8700bc4f9
6002524 <jrgaln.m@gmail.com>
2024-04-26 14:11:05 +0200

    Componentes de TaskScreen.kt, AlertDialog modificado.

--- app/src/main/java/com/example/tfgfloppy/addTask/ui/TaskScreen.kt
+++ app/src/main/java/com/example/tfgfloppy/addTask/ui/TaskScreen.kt
@@ -14,19 +14,27 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -36,6 +44,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.Font
@@ -47,6 +56,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import com.example.tfgfloppy.R
 import com.example.tfgfloppy.addTask.ui.TaskViewModel
@@ -183,6 +193,7 @@ private fun FabDialog(align: Modifier, taskViewModel: TaskViewModel) {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AddTaskDialog(
     show: Boolean,
@@ -194,7 +205,9 @@ private fun AddTaskDialog(
         mutableStateOf("")
     }
     if (show) {
-        Dialog(onDismissRequest = { onDismiss() }) {
+        AlertDialog(onDismissRequest = { onDismiss() }, properties = DialogProperties(), modifier = Modifier.clip(
+            RoundedCornerShape(24.dp)
+        )) {
             Column(
                 Modifier
                     .fillMaxWidth()
@@ -205,15 +218,36 @@ private fun AddTaskDialog(
                 OutlinedTextField(
                     value = myTask,
                     onValueChange = { myTask = it },
-                    label = { Text(text = "Añadir tarea:", fontFamily = fontFamily) })
-                Spacer(modifier = Modifier.size(16.dp))
+                    label = { Text(text = "Añadir tarea:", fontFamily = fontFamily, fontSize = 18.sp) },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                    ))
+                Spacer(modifier = Modifier.size(7.dp))
+                HorizontalLine()
+                Spacer(modifier = Modifier.size(7.dp))
                 Button(onClick = {
                     onTaskAdded(myTask)
                     myTask = ""
-                }) {
-                    Text(text = "Añadir", fontFamily = fontFamily)
+                }, modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Text(text = "Añadir", fontFamily = fontFamily, fontSize = 18.sp)
                 }
             }
         }
     }
+}
+
+@Composable
+fun HorizontalLine(
+    color: Color = Color.LightGray,
+    height: androidx.compose.ui.unit.Dp = 1.dp
+) {
+    Box(
+        modifier = Modifier
+            .width(225.dp)
+            .height(height)
+            .background(color)
+    )
 }
