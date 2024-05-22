915e6aed47d9dbb349e178d66f790e4c8be6f735
6002524 <jrgaln.m@gmail.com>
2024-05-22 14:13:39 +0200

    Tema cambiado.

--- app/src/main/java/com/example/tfgfloppy/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tfgfloppy/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
@@ -81,7 +82,7 @@ fun AppTheme(
     val context = LocalContext.current
     val colors = when {
         (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
-            if (useDarkTheme) dynamicLightColorScheme(context)
+            if (useDarkTheme) dynamicDarkColorScheme(context)
             else dynamicLightColorScheme(context)
         }
         useDarkTheme -> DarkColors
